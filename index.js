c33e399c096e8118acfbd9e8caf0411e58878fd8
sykes836 <bernard.sykes836@gmail.com>
2018-08-16 19:23:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (driverName) {
+    return driverName.toLowerCase()
+  })
+}
+ function nameToAttributes(drivers) {
+  return drivers.map(function (driverName) {
+    const firstName = driverName.split(" ")[0];
+    const lastName = driverName.split(" ")[1];
+     return { firstName: firstName, lastName: lastName };
+  })
+}
+ function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}

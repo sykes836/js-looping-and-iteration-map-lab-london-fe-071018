bcddc932d7a1e00f02b75a23722265e3a5c94405
sykes836 <bernard.sykes836@gmail.com>
2018-08-16 19:48:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,15 +12,3 @@ function nameToAttributes(drivers) {
 })
 }
 
-function nameToAttributes(drivers) {
-  return drivers.map(function (driverName) {
-    const firstName = driverName.split(" ")[0];
-    const lastName = driverName.split(" ")[1];
-     return { firstName: firstName, lastName: lastName };
-  })
-}
- function attributesToPhrase(drivers) {
-  return drivers.map(function (driver) {
-    return `${driver.name} is from ${driver.hometown}`;
-  });
-}
